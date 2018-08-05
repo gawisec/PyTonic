ac4174120dfbd3b81f49744862f799a196923f48
agamestory <gawisec@pm.me>
2018-08-05 21:57:08 +0800

    Base file.

--- gym_calculator.py
+++ gym_calculator.py
@@ -5,7 +5,7 @@ modifiers = .09 #Percentage
 gym_dots = 5.5
 energy_per_train = 10
 happy = 4750
-stat_total = 15574504
+stat_total = 16000000 
 
 def ask_gym_input():
 
@@ -17,8 +17,26 @@ def ask_gym_input():
     if user_option(1):
         print ""
 
+#gym_gain = ((0.0000003480061091*numpy.log(happy+250)+0.000003091619094)*stat_total+0.0000682775184551527*(happy+250)-0.0301431777)*(modifiers+1)*gym_dots*energy_per_train
+#total_gain = gym_gain * 15
+
+
+#print ("Single Gains: {:,.2f}".format(gym_gain))
+#print ("Total Gains:{:,.2f}".format(total_gain))
+#test = (stat_total + total_gain)
+
+
 gym_gain = ((0.0000003480061091*numpy.log(happy+250)+0.000003091619094)*stat_total+0.0000682775184551527*(happy+250)-0.0301431777)*(modifiers+1)*gym_dots*energy_per_train
 total_gain = gym_gain * 15
-print"Single Gains: %f" % (gym_gain)
-print"Total Gains: %f" % (total_gain)
+print ("Single Gains: {:,.2f}".format(gym_gain))
+print ("Total Gains:{:,.2f}".format(total_gain))
 
+initial_gain = total_gain + stat_total
+print ("Initial Gain:{:,.2f}".format(initial_gain))
+for days in range(0, 30):
+    print days
+
+    initial_gain = total_gain + stat_total
+    gg = initial_gain + total_gain
+    print gg
+    print initial_gain
